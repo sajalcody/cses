b1bd353398c15c501bf431f413b8fddc7721b0dd
Sajal Agrawal <sajal.agrawal@juspay.in>
2020-05-29 10:31:58 +0530

    basic template

---
+++ basictemplate.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+#define lli long long int
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int t, n;
+    cin >> t;
+    while (t--){
+        
+    }
+    return 0;
+}
